7ec106148ed561faf738d654b6d86d61d31d869d
Jordan Pitlor <jpitlor@gmail.com>
2015-07-22 10:04:14 -0400

    Initial Commit

---
+++ Lincoln OvensTests/Lincoln_OvensTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lincoln_OvensTests.swift
+//  Lincoln OvensTests
+//
+//  Created by Jordan Pitlor on 7/22/15.
+//  Copyright (c) 2015 Manitowoc Foodservice. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Lincoln_OvensTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
