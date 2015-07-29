156e354096d4a449607f4aecf4d6ab949a15ee20
Jordan Pitlor <jpitlor@gmail.com>
2015-07-29 18:22:41 -0400

    In the capacity calculator, the user can now toggle between rectangular and circular pans

--- Lincoln Ovens/CapacityCalculatorView.swift
+++ Lincoln Ovens/CapacityCalculatorView.swift
@@ -10,16 +10,41 @@ import Foundation
 import UIKit
 
 public class CapacityCalculatorView: UIViewController {
+	@IBOutlet weak var panType: UISegmentedControl!
 	@IBOutlet weak var beltWidth: UITextField!
 	@IBOutlet weak var chamberLength: UITextField!
 	@IBOutlet weak var bakeTime: UITextField!
-	@IBOutlet weak var panDiameter: UITextField!
-	@IBOutlet weak var panLength: UITextField!
+	@IBOutlet weak var panDiameterOrLength: UITextField!
 	@IBOutlet weak var panWidth: UITextField!
-    
-    @IBAction func calculateButtonClicked(sender: UIButton) {
-		if errorCheck() {
+	@IBOutlet weak var widthLabel: UILabel!
+	@IBOutlet weak var diameterOrLengthLabel: UILabel!
 
+	@IBAction func calculateButtonClicked(sender: UIButton) {
+		if errorCheck() {
+			let BW = Double(beltWidth.text.toInt()!)
+			let CL = Double(chamberLength.text.toInt()!)
+			let BT = Double(bakeTime.text.toInt()!)
+			let c: CapacityCalculator
+
+			if panType.selectedSegmentIndex == 0 {
+				let PD = Double(panDiameterOrLength.text.toInt()!)
+
+				c = CapacityCalculator(beltWidth: BW, ovenCapacity: CL, bakeTime: BT, panDiameter: PD)
+			} else {
+				let PL = Double(panDiameterOrLength.text.toInt()!)
+				let PW = Double(panWidth.text.toInt()!)
+
+				c = CapacityCalculator(beltWidth: BW, ovenCapacity: CL, bakeTime: BT, panWidth: PW, panLength: PL)
+			}
+
+			let capacity: Double = c.calculateCapacity()
+			let popup: UIAlertController = UIAlertController(title: "The capacity is " + String(format: "%.1f", capacity) + " pans/hour", message: nil, preferredStyle: .Alert)
+			let OKAction = UIAlertAction(title: "OK", style: .Default) {
+				(action) in
+				// Do nothing
+			}
+			popup.addAction(OKAction)
+			self.presentViewController(popup, animated: true, completion: nil)
 		} else {
 			let popup: UIAlertController = UIAlertController(title: "Inputs Missing", message: "Please fill out all of the inputs, then try calculating the capacity again", preferredStyle: .Alert)
 			let OKAction = UIAlertAction(title: "OK", style: .Default) {
@@ -32,10 +57,31 @@ public class CapacityCalculatorView: UIViewController {
 	}
 
 	private func errorCheck() -> Bool {
-		return false
+		if panType.selectedSegmentIndex == 0 {
+			panWidth.text = "0"
+		}
+
+		return !beltWidth.text.isEmpty && !chamberLength.text.isEmpty && !bakeTime.text.isEmpty && !panDiameterOrLength.text.isEmpty && !panWidth.text.isEmpty
 	}
 
+	@IBAction func panTypeClicked(sender: AnyObject) {
+		if panType.selectedSegmentIndex == 0 {
+			widthLabel.hidden = true
+			panWidth.hidden = true
+
+			diameterOrLengthLabel.text = "Diameter"
+			panDiameterOrLength.text = ""
+		} else {
+			widthLabel.hidden = false
+			panWidth.hidden = false
+
+			diameterOrLengthLabel.text = "Length"
+			panDiameterOrLength.text = ""
+			panWidth.text = ""
+		}
+	}
+    
 	private func dismiss() {
 
-    }
+	}
 }
